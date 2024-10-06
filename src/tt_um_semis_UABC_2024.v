95d009ff9dc0172db494ef296efb55223aab5878
Rogelio <149198850+rogeermv@users.noreply.github.com>
2024-10-06 16:11:34 -0700

    Update tt_um_semis_UABC_2024.v

--- src/tt_um_semis_UABC_2024.v
+++ src/tt_um_semis_UABC_2024.v
@@ -41,6 +41,6 @@ module tt_um_semis_UABC_2024 (
     assign uio_out = 0;
     assign uio_oe  = 0;
     // List all unused inputs to prevent warnings
-    wire _unused = &{ena, clk, rst_n, uio_in,1'b0};
+    wire _unused = &{ui_in[7:2],ena, clk, rst_n, uio_in,1'b0};
 
 endmodule
